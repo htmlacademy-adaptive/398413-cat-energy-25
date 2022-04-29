aaa9573e9ac5e6592083d12843531d5894bec2fd
Svetlana Oshepkova <lana_nnov@mail.ru>
2022-04-29 12:05:38 +0300

    внесены правки в верстку

--- source/js/script.js
+++ source/js/script.js
@@ -4,12 +4,9 @@
   var menu = document.querySelector('.main-nav');
   var menuToggle = document.querySelector('.main-nav__toggle');
 
-  console.log(99);
-
-  // document.body.classList.remove('no-js');
+  menu.classList.remove('main-nav--nojs');
 
   if (menuToggle) {
-    console.log(88);
     menuToggle.addEventListener('click', function (evt) {
       if (menu) {
         // evt.currentTarget.classList.toggle('main-nav__menu-toggle--active');
